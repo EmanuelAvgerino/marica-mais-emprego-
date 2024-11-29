5b509cfa72c1a9429c19dc87459faa4111872178
ViniDantas07 <viniciusjc2kt07@gmail.com>
2024-11-29 00:04:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-let slideIndex = 0; // Índice do primeiro slide
+let slideIndex = 0;
 
 function nextSlide() {
-  const slides = document.querySelectorAll('.carousel-images img'); // Seleciona todas as imagens
+  const slides = document.querySelectorAll('.carousel-images img');
   const totalSlides = slides.length;
 
-  slideIndex++; // Vai para o próximo conjunto de 3 imagens
+  slideIndex++;
 
-  if (slideIndex >= totalSlides - 2) { // Se passar da última possibilidade de 3 imagens
-    slideIndex = 0; // Volta para o início
+  if (slideIndex >= totalSlides - 2) {
+    slideIndex = 0;
   }
 
-  // Muda a posição das imagens para mostrar apenas 3 por vez
   const carousel = document.querySelector('.carousel-images');
-  carousel.style.transform = `translateX(-${slideIndex * 33.33}%)`; // Exibe 3 imagens por vez (33.33% cada)
+  carousel.style.transform = `translateX(-${slideIndex * 33.33}%)`;
 }
 
-// Troca de slide a cada 3 segundos
 setInterval(nextSlide, 3000);
